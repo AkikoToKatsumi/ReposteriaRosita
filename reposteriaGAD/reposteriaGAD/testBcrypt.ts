edc45b96950bc7e0ec80340f3c296bd3185e3232
Akiko San <gabrielag.r.mp@gmail.com>
2025-04-13 19:45:24 -0400

    /

--- reposteriaGAD/reposteriaGAD/testBcrypt.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-const testPassword = "1234"; // Contraseña original
-const testHash = "$2a$10$4dCLLFrMYm90oqNUUElyrOY8D7Ziywl9ZrLuScM.S8Ow0PYvsmr2C"; // Hash proporcionado
-
-(async () => {
-  const isValid = await bcrypt.compare(testPassword, testHash);
-  console.log("¿La contraseña coincide con el hash?:", isValid);
-})();
